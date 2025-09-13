e91db869d2073a2929738abc6dafe01971d81635
4006 <4006@4006.com>
2025-09-13 08:53:03 +0330

    test husky

--- src/app/(pages)/(user)/news/page.tsx
+++ src/app/(pages)/(user)/news/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { NewsListPage } from "@/app/components/pages/news-list";
 
 export default function page() {
-  return <NewsListPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NewsListPage />
+    </Suspense>
+  );
 }
