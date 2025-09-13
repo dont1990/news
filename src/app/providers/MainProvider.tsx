67cbe6f33a80317caa44ee2912f371b6dd386c53
4006 <4006@4006.com>
2025-09-13 08:08:37 +0330

    test husky

--- src/app/providers/MainProvider.tsx
+++ src/app/providers/MainProvider.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { ReactQueryProvider } from "../lib/api/react-query-client";
 import { Header } from "../components/shared/header";
 import { Footer } from "../components/shared/footer";
@@ -10,10 +10,7 @@ type Props = {
 const MainProvider = ({ children }: Props) => {
   return (
     <ReactQueryProvider>
-      <Suspense fallback={'loading ...'}>
-
       <Header />
-      </Suspense>
       <main>{children}</main>
       <Footer />
     </ReactQueryProvider>
