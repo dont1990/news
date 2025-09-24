00f15cb2b4a794e72c692d764c04d9c4dbe07b3a
4006 <4006@4006.com>
2025-09-24 15:53:00 +0330

    build bug fixed

--- src/app/components/pages/home/index.tsx
+++ src/app/components/pages/home/index.tsx
@@ -8,6 +8,7 @@ import { BreakingNews } from "./breaking-news";
 import { LiveRates } from "./live-rates";
 import SportNews from "./sport-news";
 import NewspapersGrid from "./newspaper";
+import { Suspense } from "react";
 
 export default function HomePageContent() {
   return (
@@ -15,21 +16,23 @@ export default function HomePageContent() {
       <Hero />
       <HomePageIntro />
 
-      <Container>
-        <div className="grid grid-cols-1 xl:grid-cols-4 gap-6 mb-40">
-          <div className="xl:col-span-3 space-y-20">
-            {/* <FeaturedNews /> */}
-            <BreakingNews />
-            <SportNews />
-            <LiveRates />
-            <LatestNews />
-            <TechNews />
-          </div>
+      <Suspense fallback={"loading ..."}>
+        <Container>
+          <div className="grid grid-cols-1 xl:grid-cols-4 gap-6 mb-40">
+            <div className="xl:col-span-3 space-y-20">
+              {/* <FeaturedNews /> */}
+              <BreakingNews />
+              <SportNews />
+              <LiveRates />
+              <LatestNews />
+              <TechNews />
+            </div>
 
-          <HomePageSidebar />
-        </div>
-      </Container>
-      <NewspapersGrid />
+            <HomePageSidebar />
+          </div>
+        </Container>
+        <NewspapersGrid />
+      </Suspense>
     </>
   );
 }
